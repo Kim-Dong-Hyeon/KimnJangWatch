8deaec9934985abaa6718f8a102674bb45e052b2
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-08-13 03:19:15 +0900

    Add: NotificationManager 파일 추가

---
+++ KimnJangWatch/Model/NotificationManager.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationManager.swift
+//  KimnJangWatch
+//
+//  Created by 김동현 on 8/13/24.
+//
+
+import UserNotifications
+
+class NotificationManager {
+  static let shared = NotificationManager()
+  
+  private init() {}
+  
+  func scheduleNotification(at date: Date, with message: String) {
+    
+  }
+}
