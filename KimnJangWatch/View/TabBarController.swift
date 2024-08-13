d3a0f056bdc1d2d8b68d89be4a171432eccb90a7
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-08-13 16:39:16 +0900

    Feat: 알림 테스트용 TestNotificationViewController 생성

--- KimnJangWatch/View/TabBarController.swift
+++ KimnJangWatch/View/TabBarController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    let testNotificationVC = UINavigationController(rootViewController: TestNotificationViewController())
+    
+    testNotificationVC.tabBarItem = UITabBarItem(title: "알림 Test", image: UIImage(systemName: "globe"), selectedImage: UIImage(systemName: "globe"))
+    
+    self.setViewControllers([testNotificationVC], animated: true)
+    
   }
-
-
+  
+  
 }
 
---
+++ KimnJangWatch/View/TestNotificationViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TestNotificationViewController.swift
+//  KimnJangWatch
+//
+//  Created by 김동현 on 8/13/24.
+//
+
+import UIKit
+
+class TestNotificationViewController: UIViewController {
+  
+}
