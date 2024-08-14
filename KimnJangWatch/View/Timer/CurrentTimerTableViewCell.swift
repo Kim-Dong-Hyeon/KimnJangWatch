9b063bf9bf66864eda97dee5cb59d78d8c412bdc
kseunghui@gmail.com <kseunghui@gmail.com>
2024-08-14 14:05:26 +0900

    Refactor: 코드 리팩토링

--- KimnJangWatch/View/Timer/CurrentTimerTableViewCell.swift
+++ KimnJangWatch/View/Timer/CurrentTimerTableViewCell.swift
@@ -8,5 +8,62 @@
 import UIKit
 
 class CurrentTimerTableViewCell: UITableViewCell {
+  static let id = "currentTimerViewCell"
+  private var isChecking = true
+  
+  private let timeLabel: UILabel = {
+    let label = UILabel()
+    label.text = "00:00"
+    label.font = .systemFont(ofSize: 30, weight: .semibold)
+    return label
+  }()
+  
+  private let startButton: UIButton = {
+    let button = UIButton()
+    button.setImage(UIImage(named: "currentStartButton"), for: .normal)
+    return button
+  }()
+  
+  private let stopButton: UIButton = {
+    let button = UIButton()
+    button.setImage(UIImage(named: "currentStopButton"), for: .normal)
+    return button
+  }()
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    setCell()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  @objc private func startButtonTapped() {
+    isChecking.toggle()
+  }
+  
+  @objc private func stopButtonTapped() {
+    isChecking.toggle()
+  }
+  
+  private func setCell() {
+    [timeLabel, startButton].forEach { contentView.addSubview($0) }
+    
+    timeLabel.snp.makeConstraints {
+      $0.centerY.equalToSuperview()
+      $0.leading.equalToSuperview().offset(10)
+    }
+    
+    startButton.snp.makeConstraints {
+      $0.trailing.equalToSuperview().offset(-10)
+      $0.width.height.equalTo(50)
+      $0.centerY.equalTo(timeLabel)
+    }
+  }
+  
+  func configCurrentCell() {
+    
+  }
 
 }
--- KimnJangWatch/View/Timer/TimerViewController.swift
+++ KimnJangWatch/View/Timer/TimerViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+
 import SnapKit
 
 class TimerViewController: UIViewController {
   
   private let scrollView = UIScrollView()
+  private let setTimerView = SetTimerViewController()
+  private let currentTimerView = CurrentTimerViewController()
+  private let recentTimerView = RecentTimerViewController()
   
   // MARK: mainStackView 및 주요 View 영역
   private var mainStackView: UIStackView = {
@@ -20,82 +24,6 @@ class TimerViewController: UIViewController {
     stackView.alignment = .fill
     stackView.distribution = .fill
     return stackView
-  }()
-  
-  private var mainTimerView: UIView = {
-    let view = UIView()
-    // view.backgroundColor = .yellow
-    return view
-  }()
-  
-  private var currentTimerView: UIView = {
-    let view = UIView()
-    // view.backgroundColor = .orange
-    return view
-  }()
-  
-  private var recentTimerView: UIView = {
-    let view = UIView()
-    // view.backgroundColor = .purple
-    return view
-  }()
-  
-  // MARK: mainTimerView UI 영역
-  private lazy var timerPicker: UIDatePicker = {
-    let timerPicker = UIDatePicker()
-    timerPicker.datePickerMode = .countDownTimer
-    return timerPicker
-  }()
-  
-  private lazy var startButton: UIButton = {
-    let button = UIButton()
-    button.setTitle("시작", for: .normal)
-    button.setTitleColor(.dangn, for: .normal)
-    button.layer.cornerRadius = 35
-    button.backgroundColor = .dangn.withAlphaComponent(0.2)
-    button.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
-    return button
-  }()
-  
-  private lazy var cancelButton: UIButton = {
-    let button = UIButton()
-    button.setTitle("취소", for: .normal)
-    button.setTitleColor(.darkGray, for: .normal)
-    button.layer.cornerRadius = 35
-    button.backgroundColor = .gray.withAlphaComponent(0.5)
-    button.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
-    return button
-  }()
-  
-  private lazy var buttonStackView: UIStackView = {
-    let stackView = UIStackView()
-    stackView.axis = .horizontal
-    stackView.spacing = 150
-    return stackView
-  }()
-  
-  // MARK: currentTimerView 영역
-  private lazy var currentTableView: UITableView = {
-    let tableView = UITableView()
-    tableView.backgroundColor = .gray
-    return tableView
-  }()
-  
-  // MARK: recentTimerView 영역
-  private lazy var recentLabel: UILabel = {
-    let label = UILabel()
-    label.text = "최근 항목"
-    label.font = .systemFont(ofSize: 24, weight: .bold)
-    return label
-  }()
-  
-  private lazy var recentTableView: UITableView = {
-    let tableView = UITableView()
-    tableView.register(RecentTimerTableViewCell.self, forCellReuseIdentifier: RecentTimerTableViewCell.id)
-    tableView.delegate = self
-    tableView.dataSource = self
-    tableView.backgroundColor = .gray
-    return tableView
   }()
   
   // MARK: 뷰 생애주기
@@ -110,10 +38,6 @@ class TimerViewController: UIViewController {
     
   }
   
-  @objc func startButtonTapped() {
-    
-  }
-  
   @objc func cancelButtonTapped() {
     
   }
@@ -125,7 +49,10 @@ class TimerViewController: UIViewController {
     self.navigationItem.largeTitleDisplayMode = .automatic
     self.title = "타이머"
     
-    let editButton = UIBarButtonItem(title: "편집", style: .plain, target: self, action: #selector(editButtonTapped))
+    let editButton = UIBarButtonItem(title: "편집",
+                                     style: .plain,
+                                     target: self,
+                                     action: #selector(editButtonTapped))
     editButton.tintColor = UIColor.dangn
     self.navigationItem.leftBarButtonItem = editButton
   }
@@ -134,13 +61,10 @@ class TimerViewController: UIViewController {
     view.addSubview(scrollView)
     scrollView.addSubview(mainStackView)
     
-    setMainTimerView()
-    setRecentTimerView()
-    setCurrentTimerView()
-    
-    [mainTimerView,
+    [setTimerView,
      currentTimerView,
-     recentTimerView].forEach { mainStackView.addArrangedSubview($0) }
+     recentTimerView].forEach {
+      addChildViewController(childContoller: $0, stackView: mainStackView) }
     
     scrollView.snp.makeConstraints {
       $0.edges.equalTo(view.safeAreaLayoutGuide)
@@ -150,84 +74,14 @@ class TimerViewController: UIViewController {
       $0.edges.equalTo(scrollView)
       $0.width.equalTo(scrollView.snp.width)
     }
-    
-    mainTimerView.snp.makeConstraints {
-      $0.height.equalTo(350)
-    }
-    
-    currentTimerView.snp.makeConstraints {
-      $0.height.equalTo(350)
-    }
-    
-    recentTimerView.snp.makeConstraints {
-      $0.height.equalTo(350)
-    }    
   }
   
-  private func setMainTimerView() {
-    mainTimerView.addSubview(timerPicker)
-    mainTimerView.addSubview(buttonStackView)
-    [cancelButton, startButton].forEach { buttonStackView.addArrangedSubview($0) }
-    
-    timerPicker.snp.makeConstraints {
-      $0.top.equalToSuperview()
-      $0.centerX.equalToSuperview()
+  private func addChildViewController(childContoller: UIViewController, stackView: UIStackView) {
+    addChild(childContoller)
+    stackView.addArrangedSubview(childContoller.view)
+    childContoller.didMove(toParent: self)
+    childContoller.view.snp.makeConstraints {
+      $0.height.equalTo(500)
     }
-    
-    cancelButton.snp.makeConstraints {
-      $0.width.height.equalTo(70)
-    }
-
-    startButton.snp.makeConstraints {
-      $0.width.height.equalTo(70)
-    }
-    
-    buttonStackView.snp.makeConstraints {
-      $0.top.equalTo(timerPicker.snp.bottom).offset(20)
-      $0.centerX.equalToSuperview()
-    }
-  }
-  
-  private func setCurrentTimerView() {
-    currentTimerView.addSubview(currentTableView)
-    currentTableView.snp.makeConstraints {
-      $0.edges.equalToSuperview()
-    }
-  }
-  
-  private func setRecentTimerView() {
-    [recentLabel, recentTableView].forEach { recentTimerView.addSubview($0)}
-    
-    recentLabel.snp.makeConstraints {
-      $0.top.equalToSuperview()
-      $0.leading.equalToSuperview().offset(20)
-    }
-    
-    recentTableView.snp.makeConstraints {
-      $0.top.equalTo(recentLabel.snp.bottom).offset(10)
-      $0.leading.trailing.equalToSuperview().inset(20)
-      $0.bottom.equalToSuperview().offset(-10)
-    }
-    
-  }
-}
-
-extension TimerViewController: UITableViewDelegate {
-  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 100
-  }
-}
-
-extension TimerViewController: UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 5
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    guard let cell = recentTableView
-      .dequeueReusableCell(withIdentifier: RecentTimerTableViewCell.id, for: indexPath) as? RecentTimerTableViewCell else {
-      return UITableViewCell()
-    }
-    return cell
   }
 }
